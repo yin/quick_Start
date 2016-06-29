faf74a5f77962af5e94e46da85a1dfeb29f50058
Matej 'Yin' Gagyi <yinotaurus@gmail.com>
2016-06-30 01:11:24 +0200

    Typescript quick start tutorial, step 3: Classes

--- quick_Start/greeter.ts
+++ quick_Start/greeter.ts
@@ -1,12 +1,19 @@
+class Student {
+    fullName: string;
+    constructor(public firstName, public lastName, public middleName) {
+	this.fullName = firstName + ' ' + middleName + ' ' + lastName
+    }
+}
+
 interface Person {
     firstName : string,
     lastName  : string
-};
+}
 
 function greeter(person : Person) {
     return "Hello, " + person.firstName + " " + person.lastName + "!"
-};
+}
 
-var user = { firstName: "Matej", lastName: "Yin" };
+var user = new Student("Matej", "Gagyi", "'Yin'")
 
-document.body.innerHTML = greeter(user);
+document.body.innerHTML = greeter(user)
